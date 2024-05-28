18715a98f30c35e517d31792df0a0cdc5cce1122
Guillermo García <gui.gaib.mex@gmail.com>
2024-05-28 12:51:01 -0600

    removed getOutputFixingParser

--- src/lib/utils/output-fixing-parser.ts
+++ src/lib/utils/output-fixing-parser.ts
@@ -3,24 +3,7 @@ import {
   StructuredOutputParser,
 } from 'langchain/output_parsers';
 import { z } from 'zod';
-import type { BaseLanguageModel } from '@langchain/core/language_models/base';
-import type { BaseOutputParser } from '@langchain/core/output_parsers';
 import { OpenAI } from '@langchain/openai';
-
-/**
- * Returns an instance of OutputFixingParser that fixes the output of a given parser using a language model.
- *
- * @template T - The type of the output.
- * @param llm - The BaseLanguageModel instance used for fixing the output.
- * @param parser - The BaseOutputParser instance used for parsing the output.
- * @returns An instance of OutputFixingParser.
- */
-export function getOutputFixingParser<T>(
-  llm: BaseLanguageModel,
-  parser: BaseOutputParser<T>
-): OutputFixingParser<T> {
-  return OutputFixingParser.fromLLM(llm, parser);
-}
 
 /**
  * Returns a structured output fixing parser based on the provided schema.
@@ -34,6 +17,7 @@ export function getStructuredOutputFixingParser<T extends z.ZodTypeAny>(
   const baseParser = StructuredOutputParser.fromZodSchema(schema);
   const llm = new OpenAI({
     model: 'gpt-4o',
+    temperature: 0,
   });
-  return getOutputFixingParser(llm, baseParser);
+  return OutputFixingParser.fromLLM(llm, baseParser);
 }
