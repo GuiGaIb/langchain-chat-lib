0358f75d094fa6b9009f44f737904b80d1cd2c5a
Guillermo García <gui.gaib.mex@gmail.com>
2024-05-30 20:49:10 -0600

    added WithRequired, WithRequiredAndNoOptional, OptionalKeys

--- src/lib/utils/utility-types.d.ts
+++ src/lib/utils/utility-types.d.ts
@@ -11,3 +11,36 @@ export type LCRunInput<T extends Runnable> = Parameters<T['invoke']>[0];
  * @template T - The type of the `Runnable`
  */
 export type LCRunOutput<T extends Runnable> = Awaited<ReturnType<T['invoke']>>;
+
+/**
+ * A type that makes specified keys in `T` required and leaves the rest as optional.
+ *
+ * @template T - The object type to be modified.
+ * @template Keys - The keys of `T` that should be required.
+ */
+export type WithRequired<
+  T extends { [key: string]: any },
+  Keys extends keyof T
+> = { [K in Keys]-?: T[K] } & { [K in Exclude<keyof T, Keys>]?: T[K] };
+
+/**
+ * A type that makes specified keys in `T` required and strips all optional properties.
+ *
+ * @template T - The object type to be modified.
+ * @template Keys - The keys of `T` that should be required.
+ */
+export type WithRequiredAndNoOptional<
+  T extends { [key: string]: any },
+  Keys extends keyof T
+> = {
+  [K in Keys]-?: T[K];
+} & { [K in Exclude<keyof T, Keys | OptionalKeys<T>>]: T[K] };
+
+/**
+ * A utility type that extracts the keys of `T` that are optional.
+ *
+ * @template T - The object type to extract optional keys from.
+ */
+export type OptionalKeys<T> = {
+  [K in keyof T]-?: {} extends Pick<T, K> ? K : never;
+}[keyof T];
