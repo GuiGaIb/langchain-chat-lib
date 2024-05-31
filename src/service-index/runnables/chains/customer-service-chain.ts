b7f47266a629181b56ccd626c0073b1ba91a59b1
Guillermo García <gui.gaib.mex@gmail.com>
2024-05-31 13:37:24 -0600

    created

---
+++ src/service-index/runnables/chains/customer-service-chain.ts
@@ -0,0 +1,103 @@
+import {
+  Runnable,
+  RunnableMap,
+} from '@langchain/core/runnables';
+
+import {
+  MongoChatSessionMemory,
+  MongoChatSessionMemoryInput,
+} from '../../../lib/memory/mongodb/mongo-chat-session-memory.js';
+import { unpackChatMemoryWithSummary } from '../../../lib/memory/utils/unpack-memory.js';
+import { LCRunOutput } from '../../../lib/utils/utility-types.js';
+import { getConversationStagesStringified } from '../tasks/get-conversation-stages-stringified.js';
+import { getConversationStage } from '../tasks/get-conversation-stage.js';
+import { getConversationStageInstructions } from '../tasks/get-conversation-stage-instructions.js';
+import { getPossibleServices } from '../tasks/get-possible-services.js';
+import { ServiceIndexDAO } from '../../db/service-index-dao.js';
+import { knowledgeRetriever } from '../retrievers/knowledge-retriever.js';
+import { AIMessage, BaseMessage } from '@langchain/core/messages';
+import { generateResponse } from '../tasks/generate-response.js';
+
+const conversation_stages_str = getConversationStagesStringified.pipe(
+  (output) => output.conversation_stages_str
+);
+const stage_instructions = getConversationStage
+  .pipe(getConversationStageInstructions)
+  .pipe((output) => output.stage_instructions);
+
+const possible_services_str = getPossibleServices
+  .pipe((output) => output.possible_services)
+  .pipe(ServiceIndexDAO.Service.getServicesLongByNames);
+
+const applicable_knowledge_str = knowledgeRetriever.pipe((output) =>
+  output.join('\n')
+);
+
+export const CustomerServiceChain: CustomerServiceChain = RunnableMap.from<
+  MongoChatSessionMemoryInput & {
+    cs_rep_name: string;
+    company_name: string;
+    company_business: string;
+  },
+  LCRunOutput<typeof unpackChatMemoryWithSummary> & {
+    cs_rep_name: string;
+    company_name: string;
+    company_business: string;
+    conversation_stages_str: string;
+  }
+>({
+  company_business: (input) => input.company_business,
+  company_name: (input) => input.company_name,
+  cs_rep_name: (input) => input.cs_rep_name,
+  memory: (input) => new MongoChatSessionMemory(input),
+  chat_history: (input) => new MongoChatSessionMemory(input).getMessages(),
+  chat_summary: (input) => new MongoChatSessionMemory(input).getSummary(),
+  chat_summary_text: (input) =>
+    new MongoChatSessionMemory(input).getSummaryAsText(),
+  conversation_stages_str: getConversationStagesStringified.pipe(
+    (output) => output.conversation_stages_str
+  ),
+})
+  .pipe(
+    RunnableMap.from<
+      LCRunOutput<typeof unpackChatMemoryWithSummary> & {
+        cs_rep_name: string;
+        company_name: string;
+        company_business: string;
+        conversation_stages_str: string;
+      },
+      {
+        cs_rep_name: string;
+        company_name: string;
+        company_business: string;
+        conversation_stages_str: string;
+        stage_instructions: string;
+        possible_services_str: string;
+        applicable_knowledge_str: string;
+        chat_summary_text: string;
+        chat_history: BaseMessage[];
+      }
+    >({
+      company_business: (input) => input.company_business,
+      company_name: (input) => input.company_name,
+      cs_rep_name: (input) => input.cs_rep_name,
+      chat_history: (input) => input.chat_history,
+      chat_summary_text: (input) => input.chat_summary_text,
+      conversation_stages_str,
+      stage_instructions,
+      possible_services_str: possible_services_str.pipe((output) =>
+        output.join('\n')
+      ),
+      applicable_knowledge_str,
+    })
+  )
+  .pipe(generateResponse);
+
+export type CustomerServiceChain = Runnable<
+  MongoChatSessionMemoryInput & {
+    cs_rep_name: string;
+    company_name: string;
+    company_business: string;
+  },
+  AIMessage
+>;
---
+++ src/service-index/runnables/tasks/generate-response.ts
@@ -0,0 +1,60 @@
+import { BaseMessage } from '@langchain/core/messages';
+import {
+  ChatPromptTemplate,
+  MessagesPlaceholder,
+} from '@langchain/core/prompts';
+
+import { BaseOpenAI } from '../../models.js';
+
+const systemMessage = `Your name is {cs_rep_name} and you work for {company_name} as a customer service representative (CSR).
+
+{company_business}.
+
+Your role as a CSR is to assist customers seeking information about the services offered by the company, guide them, and help them contract the services they need and {company_name} provides.
+
+It is preferable to maintain a casual and friendly tone in your responses, avoiding long and complicated answers. Offer clear and concise responses. In the same language the user speaks to you, restricted to English and Spanish only.
+
+Avoid being repetitive with phrases you use in your responses, and avoid responding repeated information based on the chat history.
+
+Avoid using technical terms or jargon that the customer may not understand.
+Whenever you ask the customer a question, ensure it is clear and concise.
+
+Always consider which stage of the conversation you are in and what comes next.
+{conversation_stages_str}
+
+The stage you are in is the stage:
+{stage_instructions}
+
+Below are details about the services the customer might be requesting. You should consider these details to offer the best possible service to the customer:
+
+{possible_services_str}
+
+Additional information you might need (this may be empty):
+{applicable_knowledge_str}`;
+
+const summaryMessage = `The chat history is broken down into a summary of the messages exchanged between the user and the customer service representative, and the most recent messages in the chat.
+
+The summary of the chat history is as follows (it can be empty):
+{chat_summary_text}
+
+The most recent messages follow below:`;
+
+const promptTemplate = ChatPromptTemplate.fromMessages<{
+  cs_rep_name: string;
+  company_name: string;
+  company_business: string;
+  conversation_stages_str: string;
+  stage_instructions: string;
+  possible_services_str: string;
+  applicable_knowledge_str: string;
+  chat_summary_text: string;
+  chat_history: BaseMessage[];
+}>([
+  ['system', systemMessage],
+  ['system', summaryMessage],
+  new MessagesPlaceholder('chat_history'),
+]).withConfig({ runName: 'generate_response_prompt_template' });
+
+export const generateResponse = promptTemplate.pipe(BaseOpenAI).withConfig({
+  runName: 'generate_response',
+});
