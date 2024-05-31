df3e320026ee78a4629e6539fbc4519f48a4de01
Guillermo García <gui.gaib.mex@gmail.com>
2024-05-31 05:35:55 -0600

    example

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,3 +1,20 @@
+process.env['OPENAI_API_KEY'] =
+  'sk-proj-gAziJ3QnUXOZIsVEim7gT3BlbkFJrjEW0NZJ4mfGU45Sckj7';
+process.env['SUMMARIZATION_OPENAI_MODEL'] = 'gpt-4o';
+process.env['SUMMARIZATION_OPENAI_TEMPERATURE'] = '0.5';
+process.env['BASE_OPENAI_MODEL'] = 'gpt-3.5-turbo';
+process.env['BASE_OPENAI_TEMPERATURE'] = '0.7';
+process.env['FIXING_OPENAI_MODEL'] = 'gpt-3.5-turbo';
+
+process.env['LCCHAT_MONGO_URI'] =
+  'mongodb+srv://guigaibmex:O07gV3wqOQcSitg4@devcluster.0mxnzqg.mongodb.net/test_v2?retryWrites=true&w=majority&appName=DevCluster';
+
+process.env['LANGCHAIN_API_KEY'] =
+  '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';
+process.env['LANGCHAIN_ENDPOINT'] = 'https://api.smith.langchain.com';
+process.env['LANGCHAIN_TRACING_V2'] = 'true';
+process.env['LANGCHAIN_CALLBACKS_BACKGROUND'] = 'true';
+
 // import { HumanMessage, AIMessage } from '@langchain/core/messages';
 
 // import { inputMap } from './lib/service-index/runnables/get-conversation-stage.js';
@@ -38,42 +55,91 @@
 
 // console.log(response);
 
+/* ------------------------------------------------------------------------------------------------ */
+// import { AIMessage, HumanMessage } from '@langchain/core/messages';
+
+// const { MongoChatSessionMemory } = await import(
+//   './lib/memory/mongodb/mongo-chat-session-memory.js'
+// );
+
+// const memory = new MongoChatSessionMemory({
+//   userId: 'test',
+//   maxMessageCount: 10,
+//   messageCountToSummarize: 5,
+//   includeStaleSessions: false,
+// });
+
+// process.env['MESSAGE_DEBOUNCE_CLEANUP_TIME_MS'] = String(1000 * 60);
+// process.env['MESSAGE_DEBOUNCE_TIME_MS'] = String(1000 * 5);
+
+// const { MemoryBackedDebouncer } = await import(
+//   './lib/utils/memory-backed-debouncer.js'
+// );
+
+// const debouncer = MemoryBackedDebouncer.getInstance({
+//   memory,
+//   async onTrigger() {
+//     const messages = await this.memory.getMessages();
+//     const count = messages.length;
+//     console.log('Triggered!', 'message count:', count);
+//   },
+//   userId: memory.userId,
+// });
+
+// const startingMessages = [
+//   new HumanMessage('Hello there!'),
+//   new AIMessage('Hi! How can I help you today?'),
+//   new HumanMessage('I need help with my order.'),
+//   new AIMessage('Sure! What can I help you with?'),
+//   new HumanMessage('I need to cancel my order.'),
+//   new AIMessage(
+//     'I can help you with that. Please provide me with your order number.'
+//   ),
+//   new HumanMessage('123456'),
+// ];
+
+// for (const message of startingMessages) {
+//   await debouncer.queueMessage(message);
+// }
+
+// await stall(1000 * 5);
+// await memory.addMessage(
+//   new AIMessage('Thank you. Your order has been successfully canceled.')
+// );
+
+// await debouncer.queueMessage(new HumanMessage('Thank you!'));
+// await stall(1000);
+// await debouncer.queueMessage(
+//   new HumanMessage('Can you help me create a new order?')
+// );
+// await stall(1000 * 2);
+// await debouncer.queueMessage(new HumanMessage('I would like to see the menu.'));
+
+// await stall(1000 * 5);
+// await memory.addMessage(
+//   new AIMessage(
+//     'Of course! You will find the menu at this link: https://example.com/menu'
+//   )
+// );
+
+// await stall(1000 * 1);
+// await memory.summarizeMessages();
+// const summary = await memory.getSummaryAsText();
+// console.log('Summary:', summary);
+
+// debouncer.terminate();
+
+// await memory.ChatMessage.db.close();
+
+// function stall(ms: number) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+
+/* ------------------------------------------------------------------------------------------------ */
 import { AIMessage, HumanMessage } from '@langchain/core/messages';
-process.env['OPENAI_API_KEY'] =
-  'sk-proj-gAziJ3QnUXOZIsVEim7gT3BlbkFJrjEW0NZJ4mfGU45Sckj7';
-process.env['SUMMARIZATION_OPENAI_MODEL'] = 'gpt-4o';
-process.env['SUMMARIZATION_OPENAI_TEMPERATURE'] = '0.5';
-
-process.env['LCCHAT_MONGO_URI'] =
-  'mongodb+srv://guigaibmex:O07gV3wqOQcSitg4@devcluster.0mxnzqg.mongodb.net/test_v2?retryWrites=true&w=majority&appName=DevCluster';
-
-const { MongoChatSessionMemory } = await import(
-  './lib/memory/mongodb/mongo-chat-session-memory.js'
+const { getConversationStage } = await import(
+  './service-index/runnables/tasks/get-conversation-stage.js'
 );
-
-const memory = new MongoChatSessionMemory({
-  userId: 'test',
-  maxMessageCount: 10,
-  messageCountToSummarize: 5,
-  includeStaleSessions: false,
-});
-
-process.env['MESSAGE_DEBOUNCE_CLEANUP_TIME_MS'] = String(1000 * 60);
-process.env['MESSAGE_DEBOUNCE_TIME_MS'] = String(1000 * 5);
-
-const { MemoryBackedDebouncer } = await import(
-  './lib/utils/memory-backed-debouncer.js'
-);
-
-const debouncer = MemoryBackedDebouncer.getInstance({
-  memory,
-  async onTrigger() {
-    const messages = await this.memory.getMessages();
-    const count = messages.length;
-    console.log('Triggered!', 'message count:', count);
-  },
-  userId: memory.userId,
-});
 
 const startingMessages = [
   new HumanMessage('Hello there!'),
@@ -85,41 +151,63 @@ const startingMessages = [
     'I can help you with that. Please provide me with your order number.'
   ),
   new HumanMessage('123456'),
+  new AIMessage('Thank you. Your order has been successfully canceled.'),
+  new HumanMessage('Can you help me create a new order?'),
+  new HumanMessage('I would like to see the menu.'),
+  new HumanMessage('Never mind, you are too slow!'),
 ];
 
+const { MongoChatSessionMemory } = await import(
+  './lib/memory/mongodb/mongo-chat-session-memory.js'
+);
+
+const memory = new MongoChatSessionMemory({
+  userId: 'test',
+  maxMessageCount: 5,
+  messageCountToSummarize: 3,
+  includeStaleSessions: false,
+});
+
+await memory.ChatMessage.deleteMany({ userId: memory.userId });
+await memory.Session.deleteMany({ userId: memory.userId });
+
 for (const message of startingMessages) {
-  await debouncer.queueMessage(message);
+  await memory.addMessage(message);
 }
 
-await stall(1000 * 5);
-await memory.addMessage(
-  new AIMessage('Thank you. Your order has been successfully canceled.')
+const { unpackChatMemoryWithSummary } = await import(
+  './lib/memory/utils/unpack-memory.js'
 );
 
-await debouncer.queueMessage(new HumanMessage('Thank you!'));
-await stall(1000);
-await debouncer.queueMessage(
-  new HumanMessage('Can you help me create a new order?')
-);
-await stall(1000 * 2);
-await debouncer.queueMessage(new HumanMessage('I would like to see the menu.'));
+const unpackedMemory = await unpackChatMemoryWithSummary.invoke(memory);
 
-await stall(1000 * 5);
-await memory.addMessage(
-  new AIMessage(
-    'Of course! You will find the menu at this link: https://example.com/menu'
-  )
-);
+const response = await getConversationStage.invoke({
+  ...unpackedMemory,
+  conversation_stages_str: `1 - Greeting: Customer service representative greets the user and asks how they can help.
+2 - Identify need: Customer service must identify the user's need and ask questions to understand the problem.
+2.1 - Get need details: Customer service must ask questions to fully understand the user's need and its details.
+3 - Resolve need: Customer service must resolve the user's need and provide a solution.
+4 - Objections: Customer service must address any objections the user has.
+5 - Closing: Customer service must close the conversation.`,
+});
 
-await stall(1000 * 1);
-await memory.summarizeMessages();
-const summary = await memory.getSummaryAsText();
-console.log('Summary:', summary);
-
-debouncer.terminate();
+console.log(response);
 
 await memory.ChatMessage.db.close();
+/* ------------------------------------------------------------------------------------------------ */
 
-function stall(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+/* ------------------------------------------------------------------------------------------------ */
+// import { ChatPromptTemplate } from '@langchain/core/prompts';
+
+// const prompt = ChatPromptTemplate.fromMessages([['system', '{var1}']]);
+
+// const response = await prompt.invoke({
+//   var1: JSON.stringify({
+//     a: 1,
+//     b: '2',
+//     c: null,
+//   }),
+// });
+
+// console.log(response);
+/* ------------------------------------------------------------------------------------------------ */
